c4d9f483a9b2bb1bbbbe6e67e5db1c0ca364960d
Pavel Polcr <pavel.polcr@seznam.cz>
2021-01-04 20:14:33 +0100

    Add project files.

---
+++ Petr/Budik.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petr
+{
+    public class Budik
+    {
+         public int Hlasitost { get; protected set; }
+         public string Jmeno { get; protected set; }
+         public string Zvoneni { get; protected set; }
+
+        public Budik()
+        {
+                Hlasitost = 1;
+                Jmeno = "Budík";
+                Zvoneni = "crr crr crr";
+        }
+
+        public void Zazvon()
+        {
+                Console.WriteLine(Jmeno + " zvoní:" + Zvoneni);
+        }
+
+
+        
+    }
+}
---
+++ Petr/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Budik budik = new Budik();
+            Digibudik digi = new Digibudik();
+            budik.Zazvon();
+            digi.Zazvon();
+            Console.ReadKey();
+
+        }
+    }
+}
